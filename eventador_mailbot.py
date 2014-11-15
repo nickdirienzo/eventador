aca2f0a985bffb5d8347de66146bcf43f6f5dd84
Nick DiRienzo <me@nickdirienzo.com>
2014-11-15 14:28:30 -0500

    checking emails

---
+++ eventador_mailbot.py
@@ -0,0 +1,29 @@
+import gmail_client
+
+import os
+import time
+import threading
+
+USER = 'EVENTADOR_USER'
+PASS = 'EVENTADOR_PASS'
+
+TIME_TO_CHECK = 10 * 60  # Check every ten minutes...
+
+class Mailbot(threading.Thread):
+
+  def __init__(self):
+    threading.Thread.__init__(self)
+    self.client = gmail_client.login(os.environ.get(USER), os.environ.get(PASS))
+
+  def process_inbox(self):
+    unread = self.client.inbox().mail(unread=True)
+    import pdb
+    pdb.set_trace()
+
+  def run(self):
+    while True:
+      self.process_inbox()
+      time.sleep(TIME_TO_CHECK)
+
+if __name__ == '__main__':
+  Mailbot().start()
